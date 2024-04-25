49e13b7f0922ee31e2dba168092df95e10fb8704
Yahya Saadi <yahyasaadi1998@gmail.com>
2024-04-25 16:29:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fltr_m_001_random_meal_generator
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fltr_m_001_random_meal_generator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fltr_m_001_random_meal_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
